4f45f92b0aa43d8e198c0a6244ffb74cfb63f313
smrTofighi <mrtofxn@gmail.com>
2025-06-24 16:54:05 -0700

    Finish

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.use("/api/reports", reportRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //* Start the Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
